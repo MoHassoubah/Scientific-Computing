97f109c26cdabbfe4baf0735f9832b9e3e51893b
ahmedhussien91 <a.hussien.91@gmail.com>
2019-01-05 23:15:59 +0200

    RK 4th order

--- Assignement_proj_2/Mass-Spring+Simulation/Mass-Spring Simulation/PhysEnv.cpp
+++ Assignement_proj_2/Mass-Spring+Simulation/Mass-Spring Simulation/PhysEnv.cpp
@@ -794,6 +794,42 @@ void CPhysEnv::HeunIntegrate( float DeltaTime)
 void CPhysEnv::RK4Integrate( float DeltaTime)
 {
 	// Your Code Here
+	tParticle temp_system;
+
+	/// Local Variables ///////////////////////////////////////////////////////////
+	float		halfDeltaT;
+	///////////////////////////////////////////////////////////////////////////////
+	halfDeltaT = DeltaTime / 2.0f;
+
+	// TAKE A HALF STEP AND UPDATE VELOCITY AND POSITION - Find y at full-interval (K1)
+	IntegrateSysOverTime(m_CurrentSys, m_CurrentSys, m_TempSys[3], DeltaTime);
+
+	// COMPUTE FORCES USING THESE NEW POSITIONS AND VELOCITIES - Compute slope at full-interval 
+	ComputeForces(m_TempSys[3]);
+
+	// TAKE A HALF STEP AND UPDATE VELOCITY AND POSITION - Find y at half-interval - using forces from K2 (K2)
+	IntegrateSysOverTime(m_CurrentSys, m_CurrentSys, m_TempSys[0], halfDeltaT);
+	
+	// COMPUTE FORCES USING THESE NEW POSITIONS AND VELOCITIES - Compute slope at half-interval 
+	ComputeForces(m_TempSys[0]);
+
+	// TAKE A HALF STEP AND UPDATE VELOCITY AND POSITION - Find y at half-interval - using forces from K2 (K3)
+	IntegrateSysOverTime(m_CurrentSys, m_TempSys[0], m_TempSys[1], halfDeltaT);
+
+	// COMPUTE FORCES USING THESE NEW POSITIONS AND VELOCITIES - Compute slope at half-interval 
+	ComputeForces(m_TempSys[1]);
+
+	// TAKE A HALF STEP AND UPDATE VELOCITY AND POSITION - Find y at interval - using forces from K3 (K4)
+	IntegrateSysOverTime(m_CurrentSys, m_TempSys[1], m_TempSys[2], DeltaTime);
+
+	// COMPUTE FORCES USING THESE NEW POSITIONS AND VELOCITIES - Compute slope at full-interval 
+	ComputeForces(m_TempSys[2]);
+
+	m_TargetSys->f.x = (m_TempSys[3]->f.x + 2 * (m_TempSys[0]->f.x + m_TempSys[1]->f.x) + m_TempSys[2]->f.x) / 6;
+	m_TargetSys->f.y = (m_TempSys[3]->f.y + 2 * (m_TempSys[0]->f.y + m_TempSys[1]->f.y) + m_TempSys[2]->f.y) / 6;
+	m_TargetSys->f.z = (m_TempSys[3]->f.z + 2 * (m_TempSys[0]->f.z + m_TempSys[1]->f.z) + m_TempSys[2]->f.z) / 6;
+
+	IntegrateSysOverTime(m_CurrentSys, m_TempSys[2], m_TargetSys, DeltaTime);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
